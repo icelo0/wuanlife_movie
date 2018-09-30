7e37b80ccc0912c9a4f44b61115f36e919ce884b
icelo0 <icelo0@126.com>
2018-09-30 20:55:43 +0800

    20180930

--- src/store/getters.js
+++
@@ -1,5 +0,0 @@
-const getters = {
-  user: state => state.user,
-  types: state => state.movies.types || []
-}
-export default getters
--- src/views/layout/index.js
+++
@@ -1,5 +0,0 @@
-export { default as Navbar } from './Navbar'
-
-export { default as Levelbar } from './Levelbar'
-
-export { default as AppMain } from './AppMain'
